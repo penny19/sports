07cf62ed6d1f879f585d995e3c2c99f9e586053a
penny19 <penny19@outlook.com>
2015-11-23 22:43:36 +0800

    增加TLevel用于说明羽毛球水平

--- src/main/java/badminton/models/TLevel.java
+++ src/main/java/badminton/models/TLevel.java
@@ -1,6 +1,8 @@
 package badminton.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -9,12 +11,15 @@ import javax.persistence.*;
  */
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="t_level",catalog = "sports")
 public class TLevel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private String levelintro;
+    private String levelIntro;
 
 }
